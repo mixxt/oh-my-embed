5080653d19aca18873f04be1446d2567b447851a
Christoph Geschwind <christoph.geschwind@tixxt.com>
2024-12-09 15:01:33 +0100

    Update spotify provider after report from AppSignal: https://appsignal.com/mixxt-gmbh/sites/6227339383eb678c9043fa0c/exceptions/incidents/1/samples/timestamp/2024-12-09T11:49:38Z

--- lib/oh_my_embed/providers/spotify.rb
+++ lib/oh_my_embed/providers/spotify.rb
@@ -6,7 +6,7 @@ module OhMyEmbed
       # http://blog.embed.ly/post/45149936446/oembed-for-spotify
       # https://twitter.com/nicklas2k/status/330094611202723840
 
-      self.endpoint = 'https://embed.spotify.com/oembed/'
+      self.endpoint = 'https://embed.spotify.com/oembed'
       self.schemes = [
       '//spotify.com/*',
       '//*.spotify.com/*',
--- spec/oh_my_embed/providers/spotify_spec.rb
+++ spec/oh_my_embed/providers/spotify_spec.rb
@@ -18,23 +18,52 @@ describe OhMyEmbed::Providers::Spotify do
         expect(response.type).to eq :rich
 
         expect(response.provider_name).to eq 'Spotify'
-        expect(response.provider_url).to eq 'https://www.spotify.com'
+        expect(response.provider_url).to eq 'https://spotify.com'
 
         expect(response.url).to eq content_url
 
-        expect(response.title).to eq 'John De Sohn - Dance Our Tears Away - Radio Edit'
+        expect(response.title).to eq 'Dance Our Tears Away (feat. Kristin Amparo) - Radio Edit'
 
         expect(response.author).to be nil
 
         expect(response.thumbnail).to eq({
-          url: 'https://d3rt1990lpmkn.cloudfront.net/cover/35ff8ecde854e7c713dc4ffad2f31441e7bc1207',
+          url: 'https://image-cdn-ak.spotifycdn.com/image/ab67616d00001e02dc7755f577f76b1207e8d9f7',
           width: 300,
           height: 300,
         })
 
         expect(response.embed[:html]).to be_a String
-        expect(response.embed[:width]).to eq 300
-        expect(response.embed[:height]).to eq 380
+        expect(response.embed[:width]).to eq 456
+        expect(response.embed[:height]).to eq 152
+      end
+    end
+
+    it 'follows 308 redirect' do
+      VCR.use_cassette('spotify2') do
+        response = provider.fetch("https://open.spotify.com/episode/3wQDSWBw5zGyVQnC502Kk6?si=f0b9a6e0f4ba45a2")
+
+        expect(response).to be_a OhMyEmbed::Response
+
+        expect(response.type).to eq :video
+
+        expect(response.provider_name).to eq 'Spotify'
+        expect(response.provider_url).to eq 'https://spotify.com'
+
+        expect(response.url).to eq "https://open.spotify.com/episode/3wQDSWBw5zGyVQnC502Kk6?si=f0b9a6e0f4ba45a2"
+
+        expect(response.title).to eq 'Grüne Zukunft gestalten - Einblicke in die Garten- und Landschaftsbau- Branche'
+
+        expect(response.author).to be nil
+
+        expect(response.thumbnail).to eq({
+          url: 'https://image-cdn-ak.spotifycdn.com/image/ab67656300005f1f86dc8dce4803f4e64fede079',
+          width: 300,
+          height: 300,
+        })
+
+        expect(response.embed[:html]).to be_a String
+        expect(response.embed[:width]).to eq 624
+        expect(response.embed[:height]).to eq 351
       end
     end
   end
